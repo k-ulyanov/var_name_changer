e3234914c807a2575d9a53bdad7646158f9b7ae6
Kirill <ulyanov.kirill@protonmail.com>
2021-11-29 21:51:54 +0300

    init commit

---
+++ variable_name_changer.py
@@ -0,0 +1,23 @@
+folder = input("What's the path? Please specify the file extension: ")
+folder.replace('/', '//')
+with open(folder, "r", encoding="utf-8") as file:
+    text = file.read()
+old_variable = input("The name of your variable: ")
+new_variable = input("New variable name: ")
+template = ['=', ' =', '= ',
+            '/', ' /', '/ ',
+            '*', ' *', '* ',
+            '**', ' **', '** ',
+            '%', ' %', '% ',
+            '//', ' //', '// ',
+            '.', ' .', '. ',
+            '(', '( ', ' )', ')']
+
+new_text = text
+for temp in template:
+    new_text = new_text.replace(f"{old_variable}{temp}", f"{new_variable}{temp}")
+    new_text = new_text.replace(f"{temp}{old_variable}", f"{temp}{new_variable}")
+print (text, new_text)
+
+with open(folder, "w", encoding="utf-8") as f:
+    f.write(new_text)
